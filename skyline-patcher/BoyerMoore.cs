fe6c2f487042a0d14b3d727db5c8eb7f8062a6ed
basti564 <bastian.schwickert@gmail.com>
2021-05-14 13:33:40 +0200

    Initial commit

---
+++ skyline-patcher/BoyerMoore.cs
@@ -0,0 +1,50 @@
+﻿// 1977 Boyer–Moore string-search algorithm by Robert S. Boyer and J Strother Moore. Implemented by mjs3339
+using System;
+using System.Collections.Generic;
+
+namespace skyline_patcher
+{
+    public class BoyerMoore
+    {
+        private int[] _jumpTable;
+        private byte[] _pattern;
+        private int _patternLength;
+        public BoyerMoore()
+        {
+        }
+        public void SetPattern(byte[] pattern)
+        {
+            _pattern = pattern;
+            _jumpTable = new int[256];
+            _patternLength = _pattern.Length;
+            for (var index = 0; index < 256; index++)
+                _jumpTable[index] = _patternLength;
+            for (var index = 0; index < _patternLength - 1; index++)
+                _jumpTable[_pattern[index]] = _patternLength - index - 1;
+        }
+        public unsafe List<int> SearchAll(byte[] searchArray, int startIndex = 0)
+        {
+            var index = startIndex;
+            var limit = searchArray.Length - _patternLength;
+            var patternLengthMinusOne = _patternLength - 1;
+            var list = new List<int>();
+            fixed (byte* pointerToByteArray = searchArray)
+            {
+                var pointerToByteArrayStartingIndex = pointerToByteArray + startIndex;
+                fixed (byte* pointerToPattern = _pattern)
+                {
+                    while (index <= limit)
+                    {
+                        var j = patternLengthMinusOne;
+                        while (j >= 0 && pointerToPattern[j] == pointerToByteArrayStartingIndex[index + j])
+                            j--;
+                        if (j < 0)
+                            list.Add(index);
+                        index += Math.Max(_jumpTable[pointerToByteArrayStartingIndex[index + j]] - _patternLength + 1 + j, 1);
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ skyline-patcher/Program.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+
+namespace skyline_patcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = false;
+            bool patched = false;
+            if (args.Length == 1)
+                flag = args[0] == "--undo";
+            Console.Title = "Bastians Oculus Skyline Patcher " + (flag ? "[undo mode]" : "[debug mode]");
+            Console.WriteLine("Bastians Oculus Skyline Patcher " + Assembly.GetExecutingAssembly().GetName().Version.ToString());
+
+            RegistryKey oculusKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\Oculus VR, LLC\Oculus");
+            Console.WriteLine("Grabbing base path from registry");
+            string basePath = (string)oculusKey.GetValue("Base");
+            Console.WriteLine(oculusKey.Name + "\\base" + " --> " + (basePath ?? "null"));
+            if (basePath == null)
+                basePath = @"C:\Program Files\Oculus\sus"; // default base value
+            string clientPath = Path.Combine(basePath, "Support", "oculus-client");
+            string asarPath = Path.Combine(clientPath, "resources", "app.asar");
+            if (!File.Exists(asarPath))
+            {
+                Console.WriteLine("app.asar not at " + asarPath);
+                try
+                {
+                    Process[] clientProcess = Process.GetProcessesByName("OculusClient");
+                    Console.WriteLine("Grabbing the path from Oculus Client process " + clientProcess.FirstOrDefault().Id.ToString());
+                    clientPath = Path.GetDirectoryName(clientProcess.FirstOrDefault().MainModule.FileName);
+                    asarPath = Path.Combine(clientPath, "resources", "app.asar");
+                }
+                catch (NullReferenceException)
+                {
+                    Console.WriteLine("No Oculus Client process is currently running");
+                }
+            }
+            while (!File.Exists(asarPath))
+            {
+                Console.WriteLine("app.asar not at " + asarPath);
+                Console.Write("Manually enter path of app.asar: ");
+                asarPath = Console.ReadLine();
+                clientPath = Directory.GetParent(Path.GetDirectoryName(asarPath)).FullName;
+            }
+            Console.WriteLine("Found app.asar at " + asarPath);
+
+            byte[] isPassing = Encoding.ASCII.GetBytes("var t=this.get(e);return t?t.isPassing:null");
+            byte[] isUnknown = Encoding.ASCII.GetBytes("var t=this.get(e);return t?t.isUnknown:null");
+            byte[] isPassingPatch = Encoding.ASCII.GetBytes("return true                   /*isPassing*/");
+            byte[] isUnknownPatch = Encoding.ASCII.GetBytes("return true                   /*isUnknown*/");
+
+            byte[] asarBytes = File.ReadAllBytes(asarPath);
+            Console.WriteLine("Read " + asarBytes.Length + " bytes from app.asar");
+
+            BoyerMoore bm = new BoyerMoore();
+
+            bm.SetPattern(flag ? isPassingPatch : isPassing);
+            foreach (int offset in bm.SearchAll(asarBytes))
+            {
+                Console.WriteLine("Patching function 'isPassing' at offset " + offset);
+                foreach (int index in Enumerable.Range(0,isPassingPatch.Length))
+                    asarBytes[offset + index] = (flag) ? isPassing[index] : isPassingPatch[index];
+                patched = true;
+            }
+
+            bm.SetPattern(flag ? isUnknownPatch : isUnknown);
+            foreach (int offset in bm.SearchAll(asarBytes))
+            {
+                Console.WriteLine("Patching function 'isUnknown' at offset " + offset);
+                foreach (int index in Enumerable.Range(0, isUnknownPatch.Length))
+                    asarBytes[offset + index] = (flag) ? isUnknown[index] : isUnknownPatch[index];
+                patched = true;
+            }
+
+            try
+            {
+                foreach (Process process in Process.GetProcessesByName("OculusClient"))
+                {
+                    process.Kill();
+                    Console.WriteLine("Killed Oculus Client process " + process.Id);
+                }
+            }
+            catch (NullReferenceException)
+            {
+                // Oculus Client is not running
+            }
+
+            if (patched) {
+                File.WriteAllBytes(asarPath, asarBytes);
+                Console.WriteLine("Wrote " + asarBytes.Length + " bytes to app.asar");
+            }
+            else
+                Console.WriteLine("No need to patch (app.asar)" + (flag ? " as no patches were found. Run this progeam without \"--undo\" to apply patches." : ". Try opening this as \"skyline-patcher --undo\" to reverse already applied patches"));
+
+            Console.WriteLine("Launching Oculus Client...");
+            Process.Start(Path.Combine(clientPath, "OculusClient.exe"));
+            Thread.Sleep(5000);
+        }
+    }
+}
